69213783ec4db0d2641faca0b0ffc9eb0effed21
Bartłomiej Ciernia <cierniabartek0802@gmail.com>
2026-04-05 01:01:51 +0200

    added feel, fixed multiple attacks and added scaling on attack

--- Assets/Scripts/Text/DamageText.cs
+++ Assets/Scripts/Text/DamageText.cs
@@ -1,9 +1,19 @@
+using System;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DamageText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI damageTMP;
+
+    private void Start()
+    {
+        var offsetX = Random.Range(-.5f, .5f);
+        var offsetY = Random.Range(-.2f, .2f);
+
+        transform.position += new Vector3(offsetX, offsetY, 0f);
+    }
 
     public void SetDamageText(float damage)
     {
