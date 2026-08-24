042301ca83f5d8f6e950bc8f429cb0d12349b794
Bartłomiej Ciernia <61689132+bciernia@users.noreply.github.com>
2026-08-24 18:09:43 +0200

    added cursor and new npc text over head

---
+++ Assets/Scripts/UI/CursorUI.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CursorUI : MonoBehaviour
+{
+    private RectTransform _cursorTransform;
+
+    private void Awake()
+    {
+        _cursorTransform = GetComponent<RectTransform>();
+    }
+
+    private void OnEnable()
+    {
+        Cursor.visible = false;
+    }
+
+    private void OnDisable()
+    {
+        Cursor.visible = true;
+    }
+
+    private void Update()
+    {
+        if (Mouse.current == null)
+            return;
+
+        _cursorTransform.position =
+            Mouse.current.position.ReadValue();
+    }
+}
